780eb149f05051b5b8c43c20ca6677eabcdbcd64
sms217 <minsung0316@naver.com>
2021-05-11 19:47:43 +0900

    initial commit

--- src/Main/Stage.java
+++ src/Main/Stage.java
@@ -21,8 +21,8 @@ public class Stage extends JFrame implements Runnable{
 	
 	public Stage() {
 		panel = new JPanel();
-		setTitle("tutering name");
-		setSize(500, 500);
+		setTitle("자바게티 1807110 서민성");
+		setSize(380, 405);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -160,56 +160,3 @@ public class Stage extends JFrame implements Runnable{
 	
 	}
 }
-
-
-
-//class OnOffButton extends JLabel{
-//	
-//	private final int NUMBER;
-//	
-//	private Icon icon = new ImageIcon("src/Source/Door_Rock_B_Off.png");
-//	private boolean onOff = false;
-//	
-//	public OnOffButton(int number) {
-//		this.NUMBER = number;
-//		
-//		this.setIcon(icon);
-//		
-//		if(NUMBER<3) {
-//			this.setBounds(0, 59, 100, 70);
-//		}else if(NUMBER<6) {
-//			this.setBounds(0, 151, 100, 70);
-//		}else {
-//			this.setBounds(0, 243, 100, 70);
-//		}
-//		
-//		if((NUMBER)%3==0) {
-//			this.setBounds(26, this.getY(), 100, 70);
-//		}else if((NUMBER)%3==1) {
-//			this.setBounds(133, this.getY(), 100, 70);
-//		}else {
-//			this.setBounds(240, this.getY(), 100, 70);
-//		}
-//	}
-//	public void change() {
-//		if(onOff) {
-//			onOff = false;
-//		}else {
-//			onOff = true;
-//		}
-//		if(onOff) {
-//			icon = new ImageIcon("src/Source/Door_Rock_B_On.png");
-//		}else {
-//			icon = new ImageIcon("src/Source/Door_Rock_B_Off.png");
-//		}
-//		this.setIcon(icon);
-//	}
-//	public int getNUMBER() {
-//		return NUMBER;
-//	}
-//	public boolean isOnOff() {
-//		return onOff;
-//	}
-//	
-//	
-//}
